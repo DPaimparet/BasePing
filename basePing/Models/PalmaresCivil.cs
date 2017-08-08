1a189c125192abb8c63269bea216ff021b6c4099
Denis PAIMPARET <denis.paimparet@condorcet.be>
2017-08-08 20:34:44 +0200

    Modif joueur

---
+++ basePing/Models/PalmaresCivil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace basePing.Models
+{
+    public class PalmaresCivil
+    {
+        private string intitule;
+        private DateTime annee;
+    }
+}
